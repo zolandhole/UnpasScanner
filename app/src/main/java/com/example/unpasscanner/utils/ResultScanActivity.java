51f0d723625d3e7e5d9988bd1c9b652047a50e27
zolandhole <zolandhole@gmail.com>
2019-03-27 21:42:47 +0700

    Fix on resume

--- app/src/main/java/com/example/unpasscanner/utils/ResultScanActivity.java
+++ app/src/main/java/com/example/unpasscanner/utils/ResultScanActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
@@ -27,7 +28,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -37,7 +40,8 @@ import java.util.Map;
 import java.util.Objects;
 
 public class ResultScanActivity extends AppCompatActivity {
-    private String decripted, dateformat;
+    private static final String TAG = "ResultScanActivity";
+    private String decripted, dateformat, hasilScan;
     private MediaPlayer mp, nmp;
     private ImageView imageViewSuccess;
     private TextView textViewResultNama, textViewDoa;
@@ -45,6 +49,8 @@ public class ResultScanActivity extends AppCompatActivity {
     ArrayList<String> arrayListMac;
     Intent intentList;
     private String nimMahasiswa,idMk;
+    private String resultQR;
+    private CountDownTimer countDownTimer;
 
 
     @SuppressLint({"SetTextI18n", "ResourceAsColor", "SimpleDateFormat"})
@@ -52,84 +58,99 @@ public class ResultScanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_scan);
+//        DisplayMetrics dm = new DisplayMetrics();
+//        getWindowManager().getDefaultDisplay().getMetrics(dm);
+//        int width = dm.widthPixels;
+//        int height = dm.heightPixels;
+//        getWindow().setLayout((int)(width*.63),(int)(height*.35));
 
-        DisplayMetrics dm = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(dm);
-        int width = dm.widthPixels;
-        int height = dm.heightPixels;
-        getWindow().setLayout((int)(width*.63),(int)(height*.35));
+        initView();
+        initListener();
+        initRunning();
+        hasilDariScanner();
 
-        imageViewSuccess = findViewById(R.id.imageViewSuccess);
-        textViewResultNama = findViewById(R.id.textViewResultNama);
-        textViewDoa = findViewById(R.id.textViewDoa);
+    }
 
+    private void initListener() {
         SimpleDateFormat dtf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
         Calendar datetimeKalender = Calendar.getInstance();
         Date date= datetimeKalender.getTime();
         dateformat = dtf.format(date);
-
-        Handler handler = new Handler();
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                finish();
-            }
-        };
-        handler.removeCallbacks(runnable);
-        handler.postDelayed(runnable,3*1000);
-
+        hasilScan = Objects.requireNonNull(getIntent().getExtras()).getString("HASILSCAN");
         mp = MediaPlayer.create(this, R.raw.success);
         nmp = MediaPlayer.create(this, R.raw.unsuccess);
+    }
 
+    private void initView() {
+        imageViewSuccess = findViewById(R.id.imageViewSuccess);
+        textViewResultNama = findViewById(R.id.textViewResultNama);
+        textViewDoa = findViewById(R.id.textViewDoa);
+    }
 
-        String hasilScan = Objects.requireNonNull(getIntent().getExtras()).getString("HASILSCAN");
-        assert hasilScan != null;
-        try {
-            decripted = AESUtils.decrypt(hasilScan).trim();
-            Log.e("DECRYPT QR",decripted);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    private void kirimUserKeMain() {
+        Intent intentMain = new Intent(ResultScanActivity.this, MainActivity.class);
+        intentMain.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intentMain);
+        finish();
+    }
 
+    private void hasilDariScanner() {
         int jumlahKata = countWords(decripted);
-
-        if (!(jumlahKata == 1)){
+        if (!(jumlahKata == 2)){
+            resultQR = "gagal";
             nmp.start();
             imageViewSuccess.setImageResource(R.drawable.icon_question);
             textViewDoa.setText("Hmm !");
-            textViewDoa.setTextColor(R.color.colorPrimary);
-            textViewResultNama.setVisibility(View.GONE);
-        } else {
+            textViewResultNama.setText("QRCODE tidak dikenal");
             textViewResultNama.setVisibility(View.VISIBLE);
+            countDownTimer.start();
+        } else {
             intentList = getIntent();
             arrayListMahasiswa = (ArrayList<SerializableMahasiswa>) intentList.getSerializableExtra("LISTNIM");
             arrayListMac = intentList.getStringArrayListExtra("LISTMACADDRESS");
             idMk = intentList.getStringExtra("IDMK");
-            Log.e("SCANNER RESULT NIM", arrayListMahasiswa.get(0).getNim());
-            //Log.e("SCANNER RESULT BT",arrayListMac.get(0));
-            Log.e("NIM SCANNER", decripted);
             prosesHasil();
         }
     }
 
-    @SuppressLint({"SetTextI18n", "ResourceAsColor"})
-    private void prosesHasil() {
-        String parseHasil[] = decripted.split(" ",3);
-        //final String namaHasil = parseHasil[1];
-        final String nimHasil = parseHasil[0];
-        //final String waktuHasil = parseHasil[2];
+    private void initRunning() {
+        assert hasilScan != null;
+        try {
+            decripted = AESUtils.decrypt(hasilScan).trim();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        final String dataNim = arrayListMahasiswa.get(0).getNim();
-        assert dataNim != null;
+        countDownTimer = new CountDownTimer(3000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {}
+            @Override
+            public void onFinish() {
+                switch (resultQR){
+                    case "gagal":
+                        kirimUserKeMain();
+                        break;
+                }
+            }
+        };
+    }
+
+    private void prosesHasil() {
+
+        String parseHasil[] = decripted.split(" ",2);
+        final String nimHasil = parseHasil[0];
+        final String waktuHasil = parseHasil[1];
+
+
 
         String dataakhir="";
         String namaMahasiswa="";
         for(int index1=0;index1<arrayListMahasiswa.size();index1++){
-            Log.e("MAC LIST MAHASISWA",arrayListMahasiswa.get(index1).getMac_user());
-            Log.e("NIM LIST MAHASISWA",arrayListMahasiswa.get(index1).getNim());
+//            Log.e("MAC LIST MAHASISWA",arrayListMahasiswa.get(index1).getMac_user());
+//            Log.e("NIM LIST MAHASISWA",arrayListMahasiswa.get(index1).getNim());
             if(arrayListMahasiswa.get(index1).getNim().equals(decripted)){
                 for(int index2=0;index2<arrayListMac.size();index2++){
-                    Log.e("MAC LIST DISCOVER",arrayListMac.get(index2));
+//                    Log.e("MAC LIST DISCOVER",arrayListMac.get(index2));
                     if(arrayListMac.get(index2).equals(arrayListMahasiswa.get(index1).getMac_user())){
                         dataakhir="absen";
                         nimMahasiswa=arrayListMahasiswa.get(index1).getNim();
@@ -137,42 +158,28 @@ public class ResultScanActivity extends AppCompatActivity {
                 }
             }
         }
-        Log.e("HASIL",dataakhir);
-        if(dataakhir.equals("absen")){
-            //waktu belum
-            textViewResultNama.setText(namaMahasiswa);
-            absensiMahasiswa();
-            mp.start();
+//        Log.e("HASIL",dataakhir);
+//        if(dataakhir.equals("absen")){
+//            textViewResultNama.setText(namaMahasiswa);
+//            absensiMahasiswa();
+//            mp.start();
 //            Intent intent = new Intent(this, MainActivity.class);
 //            startActivity(intent);
-        }else{
-            imageViewSuccess.setImageResource(R.drawable.icon_sad_red);
-            textViewDoa.setText("Ups !");
-            textViewDoa.setTextColor(R.color.colorPrimary);
-            textViewResultNama.setText("Mac Address atau Nim Anda Tidak Terdeteksi");
-            nmp.start();
-
-        }
-        if (dataNim.toLowerCase().contains(nimHasil.toLowerCase())){
-//            if (waktuHasil.equals(dateformat)){
-//                textViewResultNama.setText(namaHasil);
-//                mp.start();
-//            } else {
-//                imageViewSuccess.setImageResource(R.drawable.icon_sad_red);
-//                textViewDoa.setText("Ups !");
-//                textViewDoa.setTextColor(R.color.colorPrimary);
-//                textViewResultNama.setText("QR Code kadaluarsa, Silahkan Generate kembali");
-//                nmp.start();
-//            }
-
-        } else {
-            imageViewSuccess.setImageResource(R.drawable.icon_sad_red);
-            textViewDoa.setText("Ups !");
-            textViewDoa.setTextColor(R.color.colorPrimary);
-            textViewResultNama.setText("Kita tidak menemukan data Anda di Matakuliah ini");
-            nmp.start();
-        }
+//        }else{
+//            imageViewSuccess.setImageResource(R.drawable.icon_sad_red);
+//            textViewDoa.setText("Ups !");
+//            textViewDoa.setTextColor(R.color.colorPrimary);
+//            textViewResultNama.setText("Mac Address atau Nim Anda Tidak Terdeteksi");
+//            nmp.start();
+//
+//        }
     }
+
+//    private SimpleDateFormat getWaktuScanner() {
+//        Date calender = Calendar.getInstance().getTime();
+//
+//        return dfJam;
+//    }
 
     private static int countWords(String input){
         if (input == null || input.isEmpty()){
